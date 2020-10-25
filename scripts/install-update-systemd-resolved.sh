5fe5ae776f6fbd3bf1a3c410f92236db4693e59a
Ivan Savushkin <ivan@savushkin.me>
2020-10-25 22:37:46 +0300

    feat: implement user session management with systemd

---
+++ scripts/install-update-systemd-resolved.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+[ "$(id -u)" != "0" ] && exec sudo "$0" "$@"
+
+EXECUTABLE=update-systemd-resolved
+DIR="/opt/update-systemd-resolved"
+
+if [ -f "/etc/openvpn/${EXECUTABLE}" ]
+then
+	echo "${EXECUTABLE} is already installed";
+else
+	echo "${EXECUTABLE} installation";
+
+  git clone -j "$(nproc)" https://github.com/jonathanio/update-systemd-resolved.git "${DIR}"
+
+  cd "${DIR}" || exit
+
+  make
+fi
