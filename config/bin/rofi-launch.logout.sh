bc8fa2bef9e6072839a2f470aa0a896f13cd26c6
Ivan Savushkin <ivan@savushkin.me>
2020-10-25 17:46:43 +0300

    feat: implement user session management with systemd

--- config/bin/rofi-launch.logout.sh
+++ config/bin/rofi-launch.logout.sh
@@ -9,13 +9,13 @@ POWER_OFF='Power off'
 option=$(echo -e "${SUSPEND}\n${LOCK_SCREEN}\n${LOGOUT}\n${REBOOT}\n${POWER_OFF}" | rofi -width 600 -dmenu -p system)
 case $option in
     "${SUSPEND}")
-        sudo  /usr/bin/systemctl syspend
+        /usr/bin/systemctl syspend
         ;;
     "${LOCK_SCREEN}")
         "${HOME}/.config/bin/lock-screen.sh"
         ;;
     "${LOGOUT}")
-        i3-nagbar -t warning -m 'Are you sure you  want to exit i3? This will end your X session.' -b 'Yes, exit i3' 'i3-msg exit'
+        /usr/bin/systemctl --user stop wm.target
         ;;
     "${REBOOT}")
         /usr/bin/systemctl reboot
