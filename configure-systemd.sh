8b59b18a683cf8223bfd9fdb7aeffaa08232ffb7
Ivan Savushkin <ivan@savushkin.me>
2020-10-25 19:44:30 +0300

    feat: implement user session management with systemd

--- configure-systemd.sh
+++ configure-systemd.sh
@@ -4,7 +4,13 @@ systemctl --user daemon-reload
 systemctl --user enable \
 i3wm.service \
 compton.service \
+setxkbmap.service \
 polybar.service \
 theming.service \
 wallpaper.service \
-tray.target
+tray.target \
+network-manager-applet.service \
+blueman-applet.service \
+udiskie.service \
+telegram-desktop.service \
+jetbrains-toolbox.service
