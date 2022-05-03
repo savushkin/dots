131114f8c406135393b9430740f25ad92f9fc42f
Ivan Savushkin <ivan@savushkin.me>
2022-05-04 00:12:32 +0300

    feat: update packages

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -7,21 +7,22 @@ export DOCKER_USER=$1
 apt update
 apt upgrade -y
 
-while read -u3 PACKAGE
-do
-  apt install --no-install-recommends --yes "${PACKAGE}"
-done 3< <(cat packages/*.packages | sort | uniq)
+#while read -u3 PACKAGE
+#do
+#  apt install --no-install-recommends --yes "${PACKAGE}"
+#done 3< <(cat packages/*.packages | sort | uniq)
 
-pip3 install pywal
+#pip3 install pywal
 
 gem install bundler
 
-npm install -g @angular/cli
 
 for INSTALL_SCRIPT in ./scripts/install-*.sh
 do
   bash "${INSTALL_SCRIPT}" -H
 done
+
+npm install -g @angular/cli
 
 apt autoclean
 apt autoremove -y
