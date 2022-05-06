77adf3fb923d6a488191386a66048a5bef372910
Ivan Savushkin <ivan@savushkin.me>
2022-05-07 00:21:35 +0300

    feat: update packages

--- scripts/install-dotnet-core.sh
+++ scripts/install-dotnet-core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGE=dotnet-sdk-3.1
+PACKAGE=dotnet-sdk-6.0
 
 if dpkg-query -W "${PACKAGE}"
 then
@@ -9,10 +9,10 @@ else
 	echo "${PACKAGE} installation";
 
   cd /tmp || exit
-	wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+	wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 	sudo apt install -y /tmp/packages-microsoft-prod.deb
 	rm -f /tmp/packages-microsoft-prod.deb
 
 	sudo apt update
-	sudo apt install -y dotnet-sdk-3.1
+	sudo apt install -y "${PACKAGE}"
 fi
